021cbb2bbef1ee9819bb3756a0c771c369251940
revaldyazura <raihanazura7@gmail.com>
2025-11-06 11:00:02 +0700

    add new image assets for avatars and products; update CORS settings for Android emulator

--- main.py
+++ main.py
@@ -46,6 +46,7 @@ app.add_middleware(
     allow_origins=[
         'http://localhost:3000',
         'http://127.0.0.1:3000',
+        'http://10.0.2.2:8000',  # Android emulator
     ],
     allow_credentials=True,  # allow cookies/authorization headers if needed
     allow_methods=['*'],
